e4fecc504fcf491c6c453d80febe71867393042a
parthibanb224 <parthiban.b@urbanrise.in>
2024-04-05 18:09:53 +0530

    Experience Updated

--- src/components/about/Info.js
+++ src/components/about/Info.js
@@ -1,13 +1,24 @@
 import React from "react";
 
 const Info = () => {
+  const calculateMonths = (startDate, endDate) => {
+    const start = new Date(startDate);
+    const end = endDate ? new Date(endDate) : new Date();
+    const diffMonths =
+      (end.getFullYear() - start.getFullYear()) * 12 +
+      (end.getMonth() - start.getMonth());
+    return diffMonths;
+  };
+  const startDate = new Date("Dec 2023");
+  const endDate = new Date();
+  const monthsOfExperience = calculateMonths(startDate, endDate);
   return (
     <div className="about-info grid">
       <div className="about-box">
         <i class="uil uil-award about-icon"></i>
 
         <h3 className="about-title">Experience</h3>
-        <span className="about-subtitle">2 Months</span>
+        <span className="about-subtitle">{monthsOfExperience} Months</span>
       </div>
 
       <div className="about-box">
